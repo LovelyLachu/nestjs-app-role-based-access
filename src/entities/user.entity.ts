670438895a72601295b66a21e4b72f9221a9fb18
Lakshmanan <lakshmanan@fortunesoftit.com>
2024-03-03 22:53:22 +0530

    entity for use and role

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Role } from './role.entity';
+
+@Schema()
+export class User extends Document {
+    @Prop({ required: true})
+    name: string;
+
+    @Prop({ required: true, unique: true })
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ type: MongooseSchema.Types.ObjectId, ref: Role.name, index: true })
+    role: MongooseSchema.Types.ObjectId;
+
+    @Prop({ default: Date.now })
+    createdAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
